6b6517e206da31a92a5053fd84999380a3bdb789
AMEMELYANOV <emelyanov.am@gmail.com>
2023-01-21 16:04:28 +0300

    Добавил интерфейсы уровня сервисов: ShowService, UserService, TicketService.

---
+++ src/main/java/ru/job4j/cinema/service/TicketService.java
@@ -0,0 +1,66 @@
+package ru.job4j.cinema.service;
+
+import ru.job4j.cinema.model.Ticket;
+
+import java.util.List;
+
+/**
+ * Сервис билетов, логика работы с билетами
+ * @see ru.job4j.cinema.model.Ticket
+ * @author Alexander Emelyanov
+ * @version 1.0
+ */
+public interface TicketService {
+
+    /**
+     * Возвращает список всех билетов
+     *
+     * @return {@code List<Ticket>} - список всех билетов
+     */
+    List<Ticket> findAll();
+
+    /**
+     * Выполняет поиск билета по идентификатору. При успешном нахождении возвращает
+     * билет, иначе выбрасывает исключение.
+     *
+     * @param id идентификатор билета
+     * @return билет при успешном нахождении
+     */
+    Ticket findById(int id);
+
+    /**
+     * Выполняет сохранение билета. При успешном сохранении возвращает
+     * сохраненный билет, иначе выбрасывается исключение.
+     *
+     * @param ticket сохраняемый билет
+     * @return билет при успешном сохранении
+     */
+    Ticket save(Ticket ticket);
+
+    /**
+     * Выполняет обновление билета.
+     *
+     * @param ticket обновляемый билет
+     * @return true при успешном обновлении билета
+     */
+    boolean update(Ticket ticket);
+
+    /**
+     * Выполняет удаление билета по идентификатору. При успешном удалении
+     * билета возвращает true, иначе выбрасывается исключение.
+     *
+     * @param id идентификатор билета
+     * @return true при успешном обновлении билета
+     */
+    boolean deleteById(int id);
+
+    /**
+     * Выполняет удаление билетов по идентификатору сеанса. При успешном
+     * удалении возвращает true, при неудачном false.
+     *
+     * @param id идентификатор сеанса
+     * @return true при успешном удалении билетов, иначе false
+     */
+    boolean deleteTicketsByShowId(int id);
+
+}
